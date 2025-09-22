ca28364ea3be238f7f05d535e34562aa126c9ef9
Wangjishi <2054606820@qq.com>
2025-09-22 14:38:10 +0800

    finish lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,28 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesNoPrime() {
+        IntList lst = IntList.of(4, 9, 15, 78, 14);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 15 -> 78 -> 14", lst.toString());
+        assertFalse(changed);
+    }
+
+    @Test
+    public void testSquarePrimesAllSame() {
+        IntList lst = IntList.of(73, 73, 73, 73, 73);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("5329 -> 5329 -> 5329 -> 5329 -> 5329", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesSmallPrimes() {
+        IntList lst = IntList.of(2, 3, 4, 5, 6);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 4 -> 25 -> 6", lst.toString());
+        assertTrue(changed);
+    }
 }
